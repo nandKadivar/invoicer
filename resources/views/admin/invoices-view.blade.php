d9a33f620f32b231df20535c9cc30ea518be86ec
nandKadivar <kadivarnand007@gmail.com>
2022-02-17 14:17:20 +0530

    Active link bug fixed

--- resources/views/admin/invoices-view.blade.php
+++ resources/views/admin/invoices-view.blade.php
@@ -5,6 +5,87 @@ Invoicer - View Invoice
 @endsection
 
 @section('content')
+<!-- ======= Sidebar ======= -->
+<aside id="sidebar" class="sidebar">
+    <ul class="sidebar-nav" id="sidebar-nav">
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="{{route('admin.dashboard')}}">
+                <i class="bi bi-grid"></i>
+                <span>Dashboard</span>
+            </a>
+        </li>
+
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="{{route('admin.customers')}}">
+                <i class="bi bi-person"></i>
+                <span>Customers</span>
+            </a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="{{route('admin.items')}}">
+                <i class="bi bi-box"></i>
+                <span>Items</span>
+            </a>
+        </li>
+
+        <br>
+
+        <li class="nav-item">
+            <a class="nav-link" href="{{route('admin.invoices')}}">
+                <i class="bi bi-receipt"></i>
+                <span>Invoices</span>
+            </a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="{{route('admin.recurring-invoices')}}">
+                <i class="bi bi-receipt-cutoff"></i>
+                <span>Recurring Invoices</span>
+            </a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="{{route('admin.payments')}}">
+                <i class="bi bi-credit-card"></i>
+                <span>Payments</span>
+            </a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="{{route('admin.expenses')}}">
+                <i class="bi bi-calculator"></i>
+                <span>Expenses</span>
+            </a>
+        </li>
+
+        <br>
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="{{route('admin.users')}}">
+                <i class="bi bi-people"></i>
+                <span>Users</span>
+            </a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="{{route('admin.reports')}}">
+                <i class="bi bi-bar-chart"></i>
+                <span>Reports</span>
+            </a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="{{route('admin.settings')}}">
+                <i class="bi bi-gear"></i>
+                <span>Settings</span>
+            </a>
+        </li>
+    </ul>
+</aside>
+<!-- End Sidebar-->
 <main id="main" class="main" style="padding: 0;">
 
     <section class="section profile" style="margin-top: -24px; over-flow: hidden;">
